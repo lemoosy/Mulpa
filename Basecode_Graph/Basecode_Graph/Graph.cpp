0740cbea7a90da166915aa8d929a4ea7bb3a1324
Léonard Lemoosy <leocraft.lemoosy@gmail.com>
2023-09-18 15:29:12 -0400

    Création des strucutres pour le basecode Graphe.

--- Basecode_Graph/Basecode_Graph/Graph.cpp
+++ Basecode_Graph/Basecode_Graph/Graph.cpp
@@ -1,1 +1,26 @@
 #include "Graph.h"
+
+Arc::Arc(int u, int v, float w)
+{
+	assert(w >= 0.0f);
+
+	u = u;
+	v = v;
+	w = w;
+}
+
+GraphNode::GraphNode()
+{
+	m_arcs = new List<Arc>();
+	m_negValency = 0;
+	m_posValency = 0;
+}
+
+Graph::Graph(int size)
+{
+	assert(size > 0);
+
+	m_size = size;
+	m_nodes = new GraphNode[size];
+}
+
--- Basecode_Graph/Basecode_Graph/Graph.h
+++ Basecode_Graph/Basecode_Graph/Graph.h
@@ -1,26 +1,54 @@
 #pragma once
 
+#include "List.h"
 #include "Settings.h"
 
-#include "List.h"
-
-
-
-class GraphNode
+/// @brief Classe représentant un arc.
+class Arc
 {
-	
+public:
+
+	/// @brief Identifiant du noeud de départ de l'arc.
+	int m_u;
+
+	/// @brief Identifiant du noeud d'arrivée de l'arc.
+	int m_v;
+
+	/// @brief Valeur du poids de l'arc.
+	float m_w;
+
+	Arc(int u, int v, float w);
 };
 
+/// @brief Classe représentant un noeud dans un graphe
+class GraphNode
+{
+public:
+
+	/// @brief Liste des arcs sortants du noeud.
+	List<Arc>* m_arcs;
+
+	/// @brief Nombre de noeuds entrants dans le noeud.
+	int m_negValency;
+
+	/// @brief Nombre de noeuds sortants du noeud.
+	int m_posValency;
+
+	GraphNode();
+};
+
+/// @brief Classe représentant un graphe.
 class Graph
 {
 private:
 
+	/// @brief Nombre de noeuds dans le graphe.
 	int m_size;
 
-
+	/// @brief Noeuds du graphe.
+	GraphNode* m_nodes;
 
 public:
 
-
+	Graph(int size);
 };
-
